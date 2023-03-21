592640170e37a71d48a423f8751fcd910cc7b6e1
paul7299 <paul7299@stud.kea.dk>
2023-03-21 14:04:07 +0100

    1st commit

---
+++ src/test/java/com/example/groceryplus/GroceryPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groceryplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
