2efb05c062d70023d09d276fbf6d30434bd703c6
paul7299 <paul7299@stud.kea.dk>
2023-03-29 11:49:12 +0200

    opdateret controlleren

--- src/main/java/com/example/groceryplus/controllers/GroceryPlusController.java
+++ src/main/java/com/example/groceryplus/controllers/GroceryPlusController.java
@@ -2,6 +2,7 @@ package com.example.groceryplus.controllers;
 import com.example.groceryplus.model.RecipeDTO;
 import com.example.groceryplus.services.GroceryPlusService;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,39 +20,26 @@ public class GroceryPlusController {
     public GroceryPlusController(GroceryPlusService groceryPlusService) {
         this.groceryPlusService = groceryPlusService;
     }
+
     @GetMapping("")
     public String index(){
         return "index";
     }
     @GetMapping("all_recipes")
-    public String allRecipes(){
+    public String allRecipes() {
         return "all_recipes";
-   /* @GetMapping("allRecipes")
-    public String allRecipes(Model model){
-        List<RecipeDTO> recipeDTOList = new ArrayList<>();
-        model.addAttribute("recipeDTOList", recipeDTOList);
-        return "allRecipes";
-    }*/
-
-    //Lavede denne her hurtig for at tjekke om http requested virkede
-    @GetMapping("allRecipes")
-    public ResponseEntity allRecipes(){
-        List<RecipeDTO> recipeDTOList = new ArrayList<>();
-        return new ResponseEntity<>(recipeDTOList, HttpStatus.OK);
     }
 
-
     @GetMapping("all_groceries")
-    public String allGroceries(){
+    public String allGroceries() {
         return "all_groceries";
     }
     @GetMapping("shopping_list")
-    public String shoppingList(){
+    public String shoppingList() {
         return "shopping_list";
     }
     @PostMapping("create_new_recipe")
-    public String addRecipe(){
+    public String addRecipe() {
         return "create_new_recipe";
     }
-
 }
