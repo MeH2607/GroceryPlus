9bd54ee1f51709cf1ef3ee231ca65c3cc63d92d6
MeH2607 <moha898h@stud.kea.dk>
2023-03-29 10:45:56 +0200

    startet på getAllGroceries

--- src/main/java/com/example/groceryplus/repositories/GroceryPlusRepository.java
+++ src/main/java/com/example/groceryplus/repositories/GroceryPlusRepository.java
@@ -6,18 +6,16 @@ import com.example.groceryplus.services.GroceryPlusException;
 import com.example.groceryplus.repositories.util.ConnectionManager;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 @Repository("GroceryPlusDB")
-public class GroceryPlusRepository implements iGroceryPlusRepository{
+public class GroceryPlusRepository implements iGroceryPlusRepository {
 
 
     //TODO Asger: Implement this class
     @Override
-    public List<Recipe> getAllRecipes(){
+    public List<Recipe> getAllRecipes() {
 
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/GroceryPlus", "root", "Tor42Am41")) {
 
@@ -29,27 +27,48 @@ public class GroceryPlusRepository implements iGroceryPlusRepository{
 
     @Override
     public List<Grocery> getAllGroceries() {
-        return null;
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/GroceryPlus", "root", "Tor42Am41")) {
+            String SQL = "SELECT * FROM Grocery";
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while (rs.next()) {
+                String name = rs.getString();
+
+            }
+
+
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
     @Override
     public void createRecipe(Recipe recipe) {
 
     }
+
     @Override
     public void createGrocery(Grocery grocery) {
 
     }
+
     @Override
     public void deleteRecipe(String name) {
     }
+
     @Override
     public void deleteGrocery(String name) {
 
     }
+
     @Override
     public void updateRecipe(Recipe recipe) {
 
     }
+
     @Override
     public List<Grocery> getShoppinglist() {
         return null;
